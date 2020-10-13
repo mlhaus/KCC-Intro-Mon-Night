2601ad4e61c8194882c8d2df93042e3da263a60d
Aaron Grimm <k0860777@student.kirkwood.edu>
2020-10-12 19:17:16 -0500

    Created a primary_colors function

--- aaron.py
+++ aaron.py
@@ -1,25 +1,30 @@
-primary=input("Enter primary color:").lower()
-secondary=input("Enter primary color:").lower()
-if(primary=="red"):
-    if(secondary=="yellow"):
-        print("When you mix red and yellow, you get orange.")
-    elif(secondary=="blue"):
-        print("When you mix red and blue, you get purple.")
+from helpers import *
+
+def primary_colors(primary, secondary):
+    if(primary=="red"):
+        if(secondary=="yellow"):
+            print("When you mix red and yellow, you get orange.")
+        elif(secondary=="blue"):
+            print("When you mix red and blue, you get purple.")
+        else:
+            print("You didn't input two primary colors.")
+    elif(primary =="blue"):
+        if(secondary=="red"):
+            print("When you mix blue and red, you get purple.")
+        elif(secondary=="yellow"):
+            print("When you mix blue and yellow, you get green.")
+        else:
+            print("You didn't input two primary colors.")
+    elif(primary=="yellow"):
+        if(secondary=="red"):
+            print("When you mix yellow and red, you get orange.")
+        elif(secondary=="blue"):
+            print("When you mix yellow and blue, you get green.")
+        else:
+            print("You didn't input two primary colors.")
     else:
-        print("You didn't input two primary colors.")
-elif(primary =="blue"):
-    if(secondary=="red"):
-        print("When you mix blue and red, you get purple.")
-    elif(secondary=="yellow"):
-        print("When you mix blue and yellow, you get green.")
-    else:
-        print("You didn't input two primary colors.")
-elif(primary=="yellow"):
-    if(secondary=="red"):
-        print("When you mix yellow and red, you get orange.")
-    elif(secondary=="blue"):
-        print("When you mix yellow and blue, you get green.")
-    else:
-        print("You didn't input two primary colors.")
-else:
-        print("You didn't input two primary colors.")
+            print("You didn't input two primary colors.")
+
+color1 = validateUserString("Pick a color",['red', 'yellow', 'blue'], True, 3, "Invalid color." )
+color2 = validateUserString("Pick another color",['red', 'yellow', 'blue'], True, 3, "Invalid color." )
+primary_colors(color1,color2 )
