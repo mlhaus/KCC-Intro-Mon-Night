1ad1fecbfbeeeef84868cb1624593f81fb64ed40
Jacob Johnson-Rocho <jacobtjohnson@gmail.com>
2020-10-12 19:17:14 -0500

    Added function to shipping code

--- jacob.py
+++ jacob.py
@@ -1,19 +1,18 @@
-#Input
+from helpers import *
 
-packageWeight = float(input("Enter weight of package as decimal "))
+def shippingCharger(packageWeight):
+    if (packageWeight > 10):
+        shippingCharges = packageWeight * 4.75
+        print("Your shipping charges are $" + format(shippingCharges, ".2f"))
+    elif(packageWeight <= 10 and packageWeight > 6):
+        shippingCharges = packageWeight * 4.00
+        print("Your shipping charges are $" + format(shippingCharges, ".2f"))
+    elif(packageWeight <= 6 and packageWeight > 2):
+        shippingCharges = packageWeight * 3.00
+        print("Your shipping charges are $" + format(shippingCharges, ".2f"))
+    elif(packageWeight <= 2 and packageWeight > 0):
+        shippingCharges = packageWeight * 1.50
+        print("Your shipping charges are $" + format(shippingCharges, ".2f"))
 
-#Process and Output
-if (packageWeight > 10):
-    shippingCharges = packageWeight * 4.75
-    print("Your shipping charges are $" + format(shippingCharges, ".2f"))
-elif(packageWeight <= 10 and packageWeight > 6):
-    shippingCharges = packageWeight * 4.00
-    print("Your shipping charges are $" + format(shippingCharges, ".2f"))
-elif(packageWeight <= 6 and packageWeight > 2):
-    shippingCharges = packageWeight * 3.00
-    print("Your shipping charges are $" + format(shippingCharges, ".2f"))
-elif(packageWeight <= 2 and packageWeight > 0):
-    shippingCharges = packageWeight * 1.50
-    print("Your shipping charges are $" + format(shippingCharges, ".2f"))
-else:
-    print("Invalid package weight!")
+packageWeight = getNum("Enter package weight: ", 0.1, 150, 3, False, "C'mon now.... get serious!" )
+shippingCharger(packageWeight)
